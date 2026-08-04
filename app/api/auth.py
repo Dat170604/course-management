6fefb477dace99d1ab76db47191411263e9fc978
Dat170604 <nguyenduydat170604@gmail.com>
2026-08-04 12:10:45 +0700

    Day 3

---
+++ app/api/auth.py
@@ -0,0 +1,59 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from app.database import SessionLocal
+from app.models import user
+from app.models.user import User
+from app.schemas.user import UserCreate, UserResponse
+from app.auth import hash_password
+
+
+router = APIRouter(
+    prefix="/auth",
+    tags=["Authentication"]
+)
+
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+
+    finally:
+        db.close()
+
+
+
+@router.post(
+    "/register",
+    response_model=UserResponse
+)
+def register(
+    user: UserCreate,
+    db: Session = Depends(get_db)
+):
+    
+    hashed_password = hash_password(
+        user.password
+    )
+    
+
+    new_user = User(
+        username=user.username,
+        email=user.email,
+        password=hashed_password
+    )
+
+    try:
+        db.add(new_user)
+        db.commit()
+        db.refresh(new_user)
+    except:
+        db.rollback()
+        raise HTTPException(
+            status_code=400,
+            detail="Email đã tồn tại")
+
+
+    return new_user
--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 
+from app.database import Base, engine
+
+from app.models.user import User
+
+from app.api import auth
+
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
+
+app.include_router(auth.router)
 
 @app.get("/")
 def root():
-    return {"message": "Course Management API"}
+    return {"message": "Hello"}
