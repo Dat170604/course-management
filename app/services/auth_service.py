28342b150d6d3fe3ecd762f970fa4c9638d5c740
Dat170604 <nguyenduydat170604@gmail.com>
2026-09-01 12:39:47 +0700

    ci + docker build

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -34,17 +34,18 @@ def login_user(email: str, password: str, db: Session):
     if not verify_password(password, user.password):
         raise HTTPException(status_code=401, detail="Email hoặc mật khẩu không đúng")
 
-        access_token = create_access_token(data={"sub": user.email})
+    access_token = create_access_token(data={"sub": user.email})
 
-        refresh_token = create_refresh_token(data={"sub": user.email})
+    refresh_token = create_refresh_token(data={"sub": user.email})
 
-        redis_client.delete(cache_key)
+    redis_client.delete(cache_key)
 
-        return {
-            "access_token": access_token,
-            "refresh_token": refresh_token,
-            "token_type": "bearer",
-        }
+    return {
+        "access_token": access_token,
+        "refresh_token": refresh_token,
+        "token_type": "bearer",
+    }
+
 
 def refresh_access_token(refresh_token: str):
 
