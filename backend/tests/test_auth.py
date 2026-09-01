80cca5f7af839de9f2d64cbf1c6594032cb2c180
Dat170604 <nguyenduydat170604@gmail.com>
2026-09-01 16:57:37 +0700

    fix ci backend working directory

--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -85,7 +85,7 @@ def test_account_login(client, student):
 def test_account_register(client, student):
     response = client.post(
         "/auth/register",
-        json={"email": "newstudent@gmail.com", "password": "newstudent"},
+        json={"username": "newstudent", "email": "newstudent@gmail.com", "password": "newstudent"},
     )
 
     assert response.status_code == 200
@@ -93,7 +93,7 @@ def test_account_register(client, student):
     assert "refresh_token" in response.json()
 
     response = client.post(
-        "/auth/register", json={"email": student.email, "password": "student"}
+        "/auth/register", json={"username": "existingstudent", "email": student.email, "password": "student"}
     )
 
     assert response.status_code == 400
