516688c74b0c13fd0aca5f96b5c0a73699e0e2a2
Dat170604 <nguyenduydat170604@gmail.com>
2026-09-03 15:13:34 +0700

    fix: configure database for alembic

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -69,7 +69,7 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    configuration = config.get_section(config.config_ini_section,{})
+    configuration = config.get_section(config.config_ini_section, {})
 
     configuration["sqlalchemy.url"] = get_database_url()
 
